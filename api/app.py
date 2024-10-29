4c8319c0a08be964fe5dc5e23c59970e1518bbf6
shu <shu.lu0316@gmail.com>
2024-10-29 18:03:24 +0800

    change movies to POST

--- api/app.py
+++ api/app.py
@@ -34,7 +34,7 @@ db = client.sample_mflix  # 獲取 sample_mflix 資料庫
 def home():
     return "<h1>hello world</h1>"
 
-@api.route("/movies", methods=['GET'])
+@api.route("/movies", methods=['POST'])
 def get_movies():
     # movie = db.movies.find_one({'title': 'Parasite'})
     # movie['_id'] = str(movie['_id'])
@@ -43,7 +43,24 @@ def get_movies():
     # BrokenPipeError: [Errno 32] Broken pipe是一個常見的錯誤，通常出現在伺服器端嘗試向客戶端寫入數據時，而客戶端已經關閉了連接。這種情況在網絡編程中是相對正常的，特別是當客戶端在伺服器完成響應之前就終止了連接。
     # 推斷應該是因為這個表有 2 萬多筆資料，改成只取 10筆就好了
 
-    movies = db.movies.find().limit(10)  # 從 'movies' 集合中獲取所有文檔
+    # 從請求中獲取 JSON 載荷
+    data = request.get_json()
+
+    # 檢查必要的參數是否存在
+    if 'page_number' not in data:
+        return {'message': 'Missing parameters page_number'}, 400
+    elif 'page_size' not in data or data['page_size'] > 20:
+        return {'message': 'page_size not Valid'}, 400
+
+    # 獲取參數
+    # 設置分頁參數
+    page_number = data['page_number']  # 第一頁
+    page_size = data.get('page_size', 10)   # 每頁顯示 10 筆
+    #
+    # # 計算跳過的文檔數
+    skip = (page_number - 1) * page_size
+
+    movies = db.movies.find().skip().limit(page_size)  # 從 'movies' 集合中獲取所有文檔
     movies_list = []
     for movie in movies:
         movie['_id'] = str(movie['_id'])  # 將 ObjectId 轉換為字符串
